d4ce8a6fc7cad5fd904942b61b79d8a8b606a23a
ninhld <luongduyninh@gmail.com>
2015-05-23 09:24:11 +0700

    update linux device drivers

--- hello_kernel_module/hello_kernel_module.c
+++ hello_kernel_module/hello_kernel_module.c
@@ -2,20 +2,16 @@
 #include<linux/init.h>
 #include<linux/module.h>
 
-<<<<<<< HEAD
-static int __init hello_init(void)
-{
-=======
+
 static int hello_init(void)
 {	
->>>>>>> 2d441b1d4aab0b1291371a4f5dce607f793bd8ac
-	printk(KERN_ALERT "Khoi tao thanh cong\n");
-	return 0;
+    printk(KERN_ALERT "Khoi tao thanh cong\n");
+    return 0;
 }
 
 static void __exit hello_exit(void)
 {
-	printk(KERN_ALERT "Ket thuc thanh cong\n");
+    printk(KERN_ALERT "Ket thuc thanh cong\n");
 }
 
 module_init(hello_init);
